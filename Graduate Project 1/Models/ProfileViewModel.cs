92630118dcc88c21b1f057d6839aa0b39511678a
Yasteel <yasteel.gunga01@gmail.com>
2023-03-07 13:04:27 +0200

    Added CRUD Functionality
    Added and connected a Database + implemented CRUD functionality

--- Graduate Project 1/Models/ProfileViewModel.cs
+++ Graduate Project 1/Models/ProfileViewModel.cs
@@ -2,15 +2,17 @@
 {
     public class ProfileViewModel
     {
-        public String Name { get; }
-        public String Surname { get; }
-        public String DOB { get; }
-        public int Age { get; }
-        public String Country { get; }
-        public String City { get; }
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Surname { get; set; }
+        public String DOB { get; set; }
+        public int Age { get; set; }
+        public String Country { get; set; }
+        public String City { get; set; }
 
         public ProfileViewModel()
         {
+            this.Id = 0;
             this.Name = "Yasteel";
             this.Surname = "Gungapursat";
             this.DOB = new DateTime(1998,8,4).ToString();
---
+++ Graduate Project 1/Models/Profiles.cs
@@ -0,0 +1,13 @@
+﻿namespace Graduate_Project_1.Models
+{
+    public class Profiles
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public String Surname { get; set; }
+        public DateTime DOB { get; set; }
+        public int Age { get; set; }
+        public String Country { get; set; }
+        public String City { get; set; }
+    }
+}
