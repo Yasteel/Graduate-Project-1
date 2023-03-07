a8a0c212309fde16030bf2dc64fa73354e3fe2b2
Yasteel <yasteel.gunga01@gmail.com>
2023-03-07 15:33:35 +0200

    Removed Use of ProfileViewModel
    removed the unnecessary ProfileViewModel file as code was adjusted to use the Profile.cs file in the Model Folder

--- Graduate Project 1/Controllers/ProfileController.cs
+++ Graduate Project 1/Controllers/ProfileController.cs
@@ -5,9 +5,17 @@ namespace Graduate_Project_1.Controllers
 {
     public class ProfileController : Controller
     {
-        private ProfileViewModel ProfileData = new ProfileViewModel();
+        private Profiles ProfileData = new Profiles();
         public IActionResult Profile()
-        {
+        {            
+            ProfileData.ID = 0;
+            ProfileData.Name = "Yasteel";
+            ProfileData.Surname = "Gungapursat";
+            ProfileData.DOB = new DateTime(1998, 8, 4);
+            ProfileData.Age = 24;
+            ProfileData.Country = "South Africa";
+            ProfileData.City = "Durban";
+
             ViewData["Profile"] = ProfileData;
             return View();
         }
